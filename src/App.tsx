015a921583a2ff9e115c46596bb7bc52c9fc5b9a
Aditya Sharma <adityasharma4510@gmail.com>
2025-08-29 17:35:30 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import LandingPage from "@/pages/LandingPage";
 import { MobileWarning } from "@/components/MobileWarning";
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ const App = () => (
           <Route path="/" element={<LandingPage />} />
           <Route path="/preview" element={<Index />} />
         </Routes>
+        <Analytics />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
